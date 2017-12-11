9d9c1650948607620bb0b744eda7c700f83253bc
pashkevich <pavelandreevsk@gmail.com>
2017-12-11 22:04:59 +0300

    TODO 3 is DONE

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -1,13 +1,15 @@
 package com.ps.beans.ctr;
 
+import com.ps.beans.ComplexBean;
+import com.ps.beans.SimpleBeanImpl;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by iuliana.cosmina on 3/26/16.
@@ -17,13 +19,22 @@ public class CIBeansTest {
 
     @Test
     public void testConfig() {
-        // ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/ctr/sample-config-01.xml");
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/ctr/sample-config-02.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/ctr/sample-config-01.xml");
+        //ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/ctr/sample-config-02.xml");
 
         logger.info(" --- All beans in context --- ");
         for(String beanName :  ctx.getBeanDefinitionNames()) {
             logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
         }
+
+        ComplexBeanImpl complexBean0 = (ComplexBeanImpl) ctx.getBean("complexBean0");
+        ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
+        assertNotNull(complexBean0.getSimpleBean());
+        assertNotNull(complexBean1.getSimpleBean());
+        ComplexBean2Impl complexBean2 = (ComplexBean2Impl) ctx.getBean("complexBean2");
+        assertNotNull(complexBean2.getSimpleBean1());
+        assertNotNull(complexBean2.getSimpleBean2());
+
 
         //TODO 3. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
     }
